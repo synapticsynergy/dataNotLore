074b2c5082fbf3874bb9ecaa3d5441c16df64685
Blake Danson <blakedanson@gmail.com>
2019-10-12 19:54:01 -0700

    add annotations.csv reader for search input

--- src/app.py
+++ src/app.py
@@ -11,22 +11,36 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 file_path = os.path.dirname(os.path.abspath(__file__))
+root_path = file_path.split("/")[0:-1]
 option = webdriver.ChromeOptions()
 option.add_argument(" — incognito")
 
 
-def main(annotation, search_terms):
+def main():
+    annotations_csv = os.path.join("/".join(root_path), "src", "annotations", "annotations.csv")
+    with open(annotations_csv, "r") as file:
+        csv_rows = file.readlines()
+        for row in csv_rows:
+            cells = row.split(",")
+            annotation = cells[0]
+            search_terms = cells[1:]
+            search_terms[-1] = search_terms[-1].strip(" \t\n\r")
+            print(annotation, search_terms)
+            request_search_terms(annotation, search_terms)
+
+
+def request_search_terms(annotation, search_terms):
     """
     Pass in the annotation and search terms, and get a list of image urls in csv with the annotation in the filename.
     """
-    root_path = file_path.split("/")[0:-1]
     csv_filename = annotation.replace(" ", "_")
     output_dir = os.path.join("/".join(root_path), "data", f"{csv_filename}.csv")
     if os.path.exists(output_dir):
         os.remove(output_dir)
     for search_term in search_terms:
-        print(f"Searching: {search_term}")
-        url = f"https://www.google.com/imghp?q={search_term}"
+        trimmed_search_term = search_term.strip(" \t\n\r")
+        print(f"Searching: {trimmed_search_term}")
+        url = f"https://www.google.com/imghp?q={trimmed_search_term}"
         chromedriver_path = os.path.join("/".join(root_path), "bin", "chromedriver")
         browser = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=option)
         browser.get(url)
@@ -39,7 +53,7 @@ def get_image_urls(annotation, browser, output_dir, csv_filename):
     urls = browser.execute_script(
         "urls = Array.from(document.querySelectorAll('.rg_di .rg_meta')).map(el=>JSON.parse(el.textContent).ou); return urls;"
     )
-    csv_format = ",\n".join(urls)
+    csv_format = "\n".join(urls)
     file = open(output_dir, "a")
     file.write(csv_format)
     file.close()
@@ -49,5 +63,8 @@ def get_image_urls(annotation, browser, output_dir, csv_filename):
 
 
 if __name__ == "__main__":
-    annotation = "ash tree"
-    main(annotation, ["ash tree fall", "ash tree winter", "ash tree spring", "ash tree summer"])
+    main()
+    # annotation = "ash tree"
+    # request_search_terms(
+    #     annotation, ["ash tree fall", "ash tree winter", "ash tree spring", "ash tree summer"]
+    # )
